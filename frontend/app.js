f959774a8e4ee4fc33d69648ecd3820915d8a2fc
ishaq-ctrl <ishaq.siddiqui@students.mq.edu.au>
2023-03-18 21:46:27 +1100

    added a frontend

---
+++ frontend/app.js
@@ -0,0 +1,31 @@
+const noteForm = document.querySelector('#note-form');
+
+noteForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const meetingDate = document.querySelector('#meeting-date').value;
+  const meetingTime = document.querySelector('#meeting-time').value;
+  const attendees = document.querySelector('#attendees').value;
+  const meetingNotes = document.querySelector('#meeting-notes').value;
+
+  // Send note data to backend API
+  const response = await fetch('/submit-note', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      meetingDate,
+      meetingTime,
+      attendees,
+      meetingNotes
+    })
+  });
+
+  
+  const confirmationPage = 
+    <h1>Note Submitted</h1>
+    <p>Thank you for submitting your meeting notes. We'll take care of the rest!</p>
+  ;
+  document.body.innerHTML = confirmationPage;
+});
